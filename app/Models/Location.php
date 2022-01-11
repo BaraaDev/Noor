4df325ca408a5dff7c55f774891e37d373044d33
baraaofficial <baraa_official@outlook.com>
2022-01-11 08:37:07 +0200

    Second Commit

---
+++ app/Models/Location.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class Location extends Model
+{
+    use InteractsWithMedia;
+    protected $dates = ['deleted_at'];
+    protected $fillable = ['name','user_id','status'];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+    public function scopeStatus($query,$arg){
+        return $query->where('status' , $arg);
+    }
+}
---
+++ database/migrations/2022_01_11_081748_create_locations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',255)->nullable();
+            $table->text('content')->nullable();
+            $table->string('map_lat',20)->nullable();
+            $table->string('map_lng',20)->nullable();
+            $table->integer('map_zoom')->nullable();
+            $table->string('status')->nullable('1');
+            $table->integer('user_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
